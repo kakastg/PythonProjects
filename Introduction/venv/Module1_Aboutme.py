40125f6ff2839abc63511e3d65725356d8d9961b
kakastg <kakastg9@gmail.com>
2020-02-17 19:28:00 +0100

    Introductory project

---
+++ Introduction/venv/Module1_Aboutme.py
@@ -0,0 +1,25 @@
+print('Hello')
+print('I can code in Python')
+print('''Here\'s a picture of a dog
+o____
+ ||||''')
+print('''My favourite animals are sheep
+o-###-
+  | |   #
+I live in Starogard Gdański
+  _|_
+ |   |
+ |#  |
+ |   |__|_
+ | # |   #|
+ |   |    |
+_|___|_#__|_''')
+born = input('What year were you born')
+born = int(born)
+agenow= 2020 - born
+age = 2025 - born
+print('In 2025, you will be',age)
+print('If you were a dog, you would be',agenow*7)
+print("A wave :")
+print("/\  "*10)
+print("  \/"*10)
