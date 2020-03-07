1c73c961eac6251380863ed6792f64e2af555ebb
kakastg <kakastg9@gmail.com>
2020-03-07 17:11:11 +0100

    Added Firefox option

--- SeleniumTest/automation.py
+++ SeleniumTest/automation.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
 
-driver = webdriver.Firefox()
+driver = webdriver.Chrome()
 driver.get("https://youtube.com")
 searchbox = driver.find_element_by_xpath('//*[@id="search"]')
 searchbox.send_keys("MrKakastg")
 
 searchButton = driver.find_element_by_xpath('//*[@id="search-icon-legacy"]')
 searchButton.click()
+print("done")
